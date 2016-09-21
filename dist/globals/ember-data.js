b778baa4c0c88e78c96f228193361c7397ecb88f
Ryunosuke Sato <tricknotes.rs@gmail.com>
2016-09-21 16:24:47 +0900

    build

--- dist/globals/ember-data.js
+++ dist/globals/ember-data.js
@@ -6,7 +6,7 @@
  * @copyright Copyright 2011-2016 Tilde Inc. and contributors.
  *            Portions Copyright 2011 LivingSocial Inc.
  * @license   Licensed under MIT license (see license.js)
- * @version   2.8.0+534577f0db
+ * @version   2.8.0+a465d9f1f0
  */
 
 var loader, define, requireModule, require, requirejs;
@@ -17473,7 +17473,7 @@ define('ember-data/transform', ['exports', 'ember'], function (exports, _ember) 
   });
 });
 define("ember-data/version", ["exports"], function (exports) {
-  exports.default = "2.8.0+534577f0db";
+  exports.default = "2.8.0+a465d9f1f0";
 });
 define("ember-inflector", ["exports", "ember", "ember-inflector/lib/system", "ember-inflector/lib/ext/string"], function (exports, _ember, _emberInflectorLibSystem, _emberInflectorLibExtString) {
 
